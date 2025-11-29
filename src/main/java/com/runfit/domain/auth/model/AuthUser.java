6502d8ca23de445c43129bf0506b0199b044a6b0
dnjsals45 <dnjsals45@naver.com>
2025-11-29 15:31:09 +0900

    feat: 회원가입, 로그인, 로그아웃 기능 추가

---
+++ src/main/java/com/runfit/domain/auth/model/AuthUser.java
@@ -0,0 +1,21 @@
+package com.runfit.domain.auth.model;
+
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public record AuthUser(
+    Long userId,
+    String username,
+    String role
+) {
+
+    public static AuthUser create(Long userId, String username, String role) {
+        return new AuthUser(userId, username, role);
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+}
---
+++ src/main/java/com/runfit/domain/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.runfit.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
